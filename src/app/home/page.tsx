69ec30fabd25e65ea90d272a076c48ab1e03219a
Mykyta Batrak <mykbatrak@gmail.com>
2023-10-30 10:09:21 -0600

    feat: added favicon

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import { LogoDark } from "@/ui/icons/LogoDark";
+import { LogoLight } from "@/ui/icons/LogoLight";
+
+export default function Home() {
+  return (
+    <main>
+      <p>Home</p>
+      <LogoLight />
+      <LogoDark />
+    </main>
+  );
+}
---
+++ src/ui/icons/LogoDark.tsx
@@ -0,0 +1,10 @@
+export function LogoDark() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
+      <rect x="2" y="2" width="20" height="20" rx="4" fill="white" />
+      <path d="M19 15L12 11L5 15L12 19L19 15Z" stroke="black" />
+      <path d="M19 9L12 5L5 9L12 13L19 9Z" stroke="black" />
+      <path d="M19 12L12 8L5 12L12 16L19 12Z" stroke="black" />
+    </svg>
+  );
+}
